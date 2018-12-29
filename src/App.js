9063cc5b771480a0f4906be608ea8336863917ea
Ario Tamaddoni <ariozone6@gmail.com>
2018-12-28 20:02:13 -0800

    Created a Movies class component and imported required modules.#2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Movies from "./components/movies";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <main className="container">
-        <h1>Hello World!</h1></main>
-    )
+        <Movies />
+      </main>
+    );
   }
 }
 
---
+++ src/components/movies.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Movies extends Component {
+  render() {
+    return <h2>Movies Component</h2>;
+  }
+}
