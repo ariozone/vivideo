d5dce196e573a87d96f5f655fb964f5d02e18826
Ario Tamaddoni <ariozone6@gmail.com>
2018-12-31 10:45:14 -0800

    Renders a bootstrap table for the movies.#2

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -2,6 +2,27 @@ import React, { Component } from "react";
 
 export default class Movies extends Component {
   render() {
-    return <h2>Movies Component</h2>;
+    return (
+      <table className="table">
+        <thead className="thead-dark">
+          <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Genre</th>
+            <th scope="col">Stock</th>
+            <th scope="col">Rate</th>
+            <th />
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Movie Title</td>
+            <td>Comedy</td>
+            <td>10</td>
+            <td>9.8</td>
+          </tr>
+        </tbody>
+      </table>
+    );
   }
 }
