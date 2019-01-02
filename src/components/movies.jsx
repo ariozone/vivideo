f8b28afb2c6e2ecd6a65ad95e9f9896840b09892
Ario Tamaddoni <ariozone6@gmail.com>
2019-01-02 13:58:45 -0800

    A delete button added to each row that deletes a movie and updates the state.#2

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -2,10 +2,19 @@ import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 
 export default class Movies extends Component {
-  state = {
-    movies: getMovies()
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: getMovies()
+    };
 
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+  handleDelete(movie) {
+    // console.log(movie);
+    const movies = [...this.state.movies].filter(m => m !== movie);
+    this.setState({ movies });
+  }
   render() {
     return (
       <table className="table">
@@ -15,6 +24,8 @@ export default class Movies extends Component {
             <th scope="col">Genre</th>
             <th scope="col">Stock</th>
             <th scope="col">Rate</th>
+            <th />
+            <th />
           </tr>
         </thead>
         <tbody>
@@ -24,6 +35,15 @@ export default class Movies extends Component {
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td />
+              <td>
+                <button
+                  className="btn btn-sm btn-danger"
+                  onClick={() => this.handleDelete(movie)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
