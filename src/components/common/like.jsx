4e761b58651f13ba371b8b56fc55fc1fa689c3f5
Ario Tamaddoni <ariozone6@gmail.com>
2019-01-04 14:55:02 -0800

    Added a click event listener to each heart.#3

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -4,6 +4,8 @@ export default class Like extends Component {
   render() {
     let classes = "fa fa-heart";
     if (!this.props.liked) classes += "-o";
-    return <i className={classes} aria-hidden="true" />;
+    return (
+      <i onClick={this.props.onClick} className={classes} aria-hidden="true" />
+    );
   }
 }
