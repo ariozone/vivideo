727694e572ab35e4cd478c05fe6b4ed626162d46
Ario Tamaddoni <ariozone6@gmail.com>
2019-01-04 14:23:50 -0800

    Added a fontawesome heart to render method of Like class component.#3

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class Like extends Component {
   render() {
-    return <h1>Hello World</h1>;
+    return <i className="far fa-heart" />;
   }
 }
