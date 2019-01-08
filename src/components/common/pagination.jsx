d769b8065dce96916b3b14563493fae23b76a5f6
Ario Tamaddoni <ariozone6@gmail.com>
2019-01-07 19:57:49 -0800

    Installed Lodash and declared a variable that stores number of pages based on number of movies per page.#11

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import _ from "lodash";
 
-export default function Pagination() {
+export default function Pagination(props) {
+  const { items, pageSize, onPageChanges } = props;
+  const numberOfPages = items / pageSize;
+  const pages = _.range(1, numberOfPages + 1);
+
   return (
     <nav aria-label="Page navigation example">
       <ul className="pagination">
