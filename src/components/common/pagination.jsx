24a68d1d0e0219f7346e4b4a069ac2562b360974
Ario Tamaddoni <ariozone6@gmail.com>
2019-01-07 19:31:11 -0800

    Enhanced pagination functional component to return a bootstrap pagination component.#11

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function Pagination() {
-  return <p>pagination</p>;
+  return (
+    <nav aria-label="Page navigation example">
+      <ul className="pagination">
+        <li className="page-item">
+          <a className="page-link">1</a>
+        </li>
+        <li className="page-item">
+          <a className="page-link">2</a>
+        </li>
+        <li className="page-item">
+          <a className="page-link">3</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
