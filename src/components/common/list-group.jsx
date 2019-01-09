313c46492ef0c4b3b670713f0390cb92090530be
Ario Tamaddoni <ariozone6@gmail.com>
2019-01-09 13:59:45 -0800

    Enhanced ListGroup functional component to return a bootstrap list-group component using passed props dynamically.#4

--- src/components/common/list-group.jsx
+++ src/components/common/list-group.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function ListGroup() {
-  return <div>List Group Here!</div>;
+export default function ListGroup(props) {
+  const { items } = props;
+  return (
+    <ul className="list-group">
+      {items.map(item => (
+        <li key={item._id} className="list-group-item">
+          {item.name}
+        </li>
+      ))}
+    </ul>
+  );
 }
