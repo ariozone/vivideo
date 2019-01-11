68985bdbde95d721fcf098abccd3a804508edf27
Ario Tamaddoni <ariozone6@gmail.com>
2019-01-11 12:47:36 -0800

    Created a movies table functional component and moved the movies table component inside it.#14

--- src/components/movies-table.jsx
+++ src/components/movies-table.jsx
@@ -1,1 +1,44 @@
 import React from "react";
+
+export default function MoviesTable(props) {
+  return (
+    <table className="table m-2">
+      <thead className="thead-dark">
+        <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Genre</th>
+          <th scope="col">Stock</th>
+          <th scope="col">Rate</th>
+          <th />
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        {movies.length > 0
+          ? movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
+                <td>
+                  <button
+                    className="btn btn-sm btn-danger"
+                    onClick={() => this.handleDelete(movie)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))
+          : null}
+      </tbody>
+    </table>
+  );
+}
