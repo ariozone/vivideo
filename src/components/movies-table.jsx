5237a2e3023dcd39a94a6bbb8532c99bb6e76770
Ario Tamaddoni <ariozone6@gmail.com>
2019-01-12 14:10:21 -0800

    Moved the raiseSort function from MoviesTable class component to TableHead class component.#14

--- src/components/movies-table.jsx
+++ src/components/movies-table.jsx
@@ -2,17 +2,6 @@ import React, { Component } from "react";
 import Like from "./common/like";
 
 export default class MoviesTable extends Component {
-  raiseSort(path) {
-    const sortColumn = { ...this.props.sortColumn };
-    if (sortColumn.path === path) {
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    } else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
-    this.props.onSort(sortColumn);
-  }
-
   render() {
     const { movies, onDelete, onLike } = this.props;
     return (
