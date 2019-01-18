3409d81a2de86e7165da78d6b0976f5656e722f5
Ario Tamaddoni <ariozone6@gmail.com>
2019-01-17 16:15:26 -0800

    Imported Movies, Customers, Rentals and Not-Found modules to App.js.#5

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import Movies from "./components/movies";
 import "./App.css";
 import { Route } from "react-router-dom";
+import Customers from "./components/customers";
+import MovieForm from "./components/movie-form";
+import NotFound from "./components/not-found";
+import Rentals from "./components/rentals";
 
 class App extends Component {
   render() {
