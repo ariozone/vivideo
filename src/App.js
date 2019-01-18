7b275846fb0fd4ce0705daaab29b39931965e561
Ario Tamaddoni <ariozone6@gmail.com>
2019-01-17 18:11:33 -0800

    In App.js, enhanced render method to render NavBar beside main tag.#5

--- src/App.js
+++ src/App.js
@@ -6,22 +6,26 @@ import Customers from "./components/customers";
 import MovieForm from "./components/movie-form";
 import NotFound from "./components/not-found";
 import Rentals from "./components/rentals";
+import NavBar from "./components/navbar";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
-        <Switch>
-          <Route path="/movies" component={Movies} />
-          <Redirect path="/" exact to="/movies" />
-          <Route path="/customers" component={Customers} />
-          <Route path="/rentals" component={Rentals} />
-          <Route path="/movie-form" component={MovieForm} />
-          <Route path="/not-found" component={NotFound} />
-          <Redirect to="/not-found" />
-        </Switch>
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          {/* <Movies /> */}
+          <Switch>
+            <Route path="/movies" component={Movies} />
+            <Redirect path="/home" exact to="/movies" />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/movie-form" component={MovieForm} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,33 +3,30 @@ import { Link, NavLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link> className="navbar-brand" to="/"> Vivideo</Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link className="navbar-brand" to="/home">
+        {" "}
+        Vivideo
+      </Link>
+
+      <div className="collapse navbar-collapse" id="navbarText">
+        <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <NavLink>
+            <NavLink className="nav-link" to="/movies">
               {" "}
-              className="nav-link" to="/movies"> Movies{" "}
-              <span className="sr-only">(current)</span>
+              Movies<span className="sr-only">(current)</span>
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink> className="nav-link" to="/customers"> Customers</NavLink>
+            <NavLink className="nav-link" to="/customers">
+              Customers
+            </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink> className="nav-link" to="/rentals"> Rentals</NavLink>
+            <NavLink className="nav-link" to="/rentals">
+              {" "}
+              Rentals
+            </NavLink>
           </li>
         </ul>
       </div>
