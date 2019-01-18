9b5d5377ead9e72ee003556cfdb510557113817d
Ario Tamaddoni <ariozone6@gmail.com>
2019-01-17 16:51:01 -0800

    Imported Link and NavLink to navbar and replaced anchor tags with Link and NavLink and all href attributes with 'to'.5

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,10 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        Navbar
-      </a>
+      <Link> className="navbar-brand" to="#"> Navbar</Link>
       <button
         className="navbar-toggler"
         type="button"
@@ -20,19 +19,17 @@ export default function NavBar() {
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <a className="nav-link" href="#">
-              Home <span className="sr-only">(current)</span>
-            </a>
+            <NavLink>
+              {" "}
+              className="nav-link" to="#"> Home{" "}
+              <span className="sr-only">(current)</span>
+            </NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
-              Features
-            </a>
+            <NavLink> className="nav-link" to="#"> Features</NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
-              Pricing
-            </a>
+            <NavLink> className="nav-link" to="#"> Pricing</NavLink>
           </li>
         </ul>
       </div>
