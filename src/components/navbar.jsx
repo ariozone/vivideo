6e1897cd6384043edcaf96379a56eaf80fea81f9
Ario Tamaddoni <ariozone6@gmail.com>
2019-01-19 18:34:42 -0800

    Added a link to login form in the navigation bar.#6

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -40,6 +40,12 @@ export default function NavBar() {
               Rentals
             </NavLink>
           </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/login">
+              {" "}
+              Login
+            </NavLink>
+          </li>
         </ul>
       </div>
     </nav>
