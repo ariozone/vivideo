f78fe39087d1fc24035073556078fdf7d4600977
Ario Tamaddoni <ariozone6@gmail.com>
2019-01-21 15:55:23 -0800

    In login-form.jsx, enhanced LoginForm render method to return a bootstrap form with two inputs.#6

--- src/components/login-form.jsx
+++ src/components/login-form.jsx
@@ -2,6 +2,36 @@ import React, { Component } from "react";
 
 export default class LoginForm extends Component {
   render() {
-    return <h1>Login</h1>;
+    return (
+      <div>
+        <h1>Login</h1>
+
+        <form>
+          <div className="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+            />
+          </div>
+          <div className="form-check" />
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
   }
 }
