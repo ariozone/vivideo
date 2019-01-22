fec61a8b5230173aff8a84fcfe9d9031b087f09a
Ario Tamaddoni <ariozone6@gmail.com>
2019-01-21 17:12:28 -0800

    Replaced the value of onChange property of input element in Input component with onChange property of props object.#6

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-export default function Form(props) {
+export default function Form({ name, label, value, handleChange }) {
   return (
     <div className="form-group">
-      <label htmlFor={props.name}>{props.label}</label>
+      <label htmlFor={name}>{label}</label>
       <input
         type="text"
         className="form-control"
-        name={props.name}
-        id={props.name}
-        value={props.value}
-        onChange={props.handleChange}
+        name={name}
+        id={name}
+        value={value}
+        onChange={handleChange}
       />
     </div>
   );
