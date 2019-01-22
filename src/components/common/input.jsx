39454095031124c64ca46e39962b0f3ed0b28d34
Ario Tamaddoni <ariozone6@gmail.com>
2019-01-21 17:09:47 -0800

    Changed the value of value property of input element in Form component with value property of props object.#6

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -3,13 +3,13 @@ import React from "react";
 export default function Form(props) {
   return (
     <div className="form-group">
-      <label htmlFor="props.name">{props.name}</label>
+      <label htmlFor={props.name}>{props.label}</label>
       <input
         type="text"
         className="form-control"
         name={props.name}
         id={props.name}
-        value={account.username}
+        value={props.value}
         onChange={props.handleChange}
       />
     </div>
