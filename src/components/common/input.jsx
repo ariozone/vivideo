d2ca93db21a5f941f33c2705d700a3d1375bfdfd
Ario Tamaddoni <ariozone6@gmail.com>
2019-01-23 15:34:03 -0800

    Improved code in Input component; used props destructuring using rest parameter and spread operator.#6

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 
-const Input = ({ name, label, value, onChange, error, type }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        type={type}
-        className="form-control"
-        name={name}
-        id={name}
-        value={value}
-        onChange={onChange}
-      />
+      <input name={name} className="form-control" id={name} {...rest} />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
