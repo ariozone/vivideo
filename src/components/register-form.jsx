22f820259fd30cd839d22db060fecad29ac2f2ea
Ario Tamaddoni <ariozone6@gmail.com>
2019-01-24 12:49:35 -0800

    Imported Joi and created an schema to validate inputs.#25

--- src/components/register-form.jsx
+++ src/components/register-form.jsx
@@ -1,18 +1,34 @@
 import React from "react";
+import Joi from "joi-browser";
 import Form from "./common/form";
 
 export default class RegisterForm extends Form {
-  state = {
-    data: { username: "", password: "" },
-    error: {}
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: { username: "", password: "", user: "" },
+      errors: {}
+    };
+    this.doSubmit = this.doSubmit.bind(this);
+  }
+  schema = {
+    username: Joi.string(),
+    password: Joi.string(),
+    user: Joi.string()
   };
+  doSubmit() {
+    console.log("Submitted!");
+  }
   render() {
     return (
       <div>
-        <h1>Register</h1>;{this.renderInput("username", "Username", "email")}
-        {this.renderInput("password", "Password", "password")}
-        {this.renderInput("name", "Name")}
-        {this.renderButton("Register")}
+        <h1>Register</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username", "email")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("user", "Name", "text")}
+          {this.renderButton("Register")}
+        </form>
       </div>
     );
   }
