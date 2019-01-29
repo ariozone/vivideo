96ff7c0846fe925f0f7dc32e199ed5271a84856b
Ario Tamaddoni <ariozone6@gmail.com>
2019-01-29 15:03:09 -0800

    Created searchInput functional component that returns a search box.#9

--- src/components/common/search-input.jsx
+++ src/components/common/search-input.jsx
@@ -1,1 +1,14 @@
 import React from "react";
+export default function SearchInput(props) {
+  return (
+    <div className="input-group mb-3">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Username"
+        aria-label="Username"
+        aria-describedby="basic-addon1"
+      />
+    </div>
+  );
+}
