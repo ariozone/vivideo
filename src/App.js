f45bc0bed6c07bacfd4517c6526877a73a824eb8
Ario Tamaddoni <ariozone6@gmail.com>
2019-05-14 14:40:44 -0700

    Install react-toastify, import its ToastContainer and its css into app.js and create a ToastContainer in render method.#33

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, { Component } from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
-import Movies from "./components/movies";
-import Customers from "./components/customers";
-import MovieForm from "./components/movie-form";
-import NotFound from "./components/not-found";
-import Rentals from "./components/rentals";
-import LoginForm from "./components/login-form";
-import RegisterForm from "./components/register-form";
-import NavBar from "./components/navbar";
-import "./App.css";
+import React, { Component } from "react"
+import { Route, Switch, Redirect } from "react-router-dom"
+import Movies from "./components/movies"
+import Customers from "./components/customers"
+import MovieForm from "./components/movie-form"
+import NotFound from "./components/not-found"
+import Rentals from "./components/rentals"
+import LoginForm from "./components/login-form"
+import RegisterForm from "./components/register-form"
+import NavBar from "./components/navbar"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import "./App.css"
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <ToastContainer />
         <NavBar />
         <main className="container">
           <Switch>
@@ -30,8 +33,8 @@ class App extends Component {
           </Switch>
         </main>
       </React.Fragment>
-    );
+    )
   }
 }
 
-export default App;
+export default App
