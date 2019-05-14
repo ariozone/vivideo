a8416ef37a9003116a296cf4484147b997bc84e2
Ario Tamaddoni <ariozone6@gmail.com>
2019-05-14 14:22:16 -0700

    Use axios interceptors to enhance http services by handling client and server errors.#33

--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -1,5 +1,16 @@
 import axios from "axios"
 
+axios.interceptors.response.use(null, error => {
+  const clientError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500
+  if (!clientError) {
+    // Log the error
+    alert("Something went wrong!")
+  }
+  return Promise.reject(error)
+})
 export default {
   get: axios.get,
   post: axios.post,
