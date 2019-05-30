5dc941e4f7199db1711a8ec508435799af40c4c6
Ario Tamaddoni <ariozone6@gmail.com>
2019-05-29 17:52:36 -0700

    In authentication module, define a function that sends user credentials objects to the logins end point.#36

--- src/services/authenticationService.js
+++ src/services/authenticationService.js
@@ -2,3 +2,7 @@ import http from "./httpServices"
 import { url } from "../config.json"
 
 const endPoint = url + "/logins"
+
+export function login(email, password) {
+  return http.post(endPoint, { email, password })
+}
