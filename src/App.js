7c19c4a28224d298b8c8a7ef0c1d9494487828b3
Ario Tamaddoni <ariozone6@gmail.com>
2019-06-06 16:08:02 -0700

    In App component replace protected routes with the new ProtectedRoute component.#38

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import RegisterForm from "./components/register-form"
 import NavBar from "./components/navbar"
 import Profile from "./components/profile"
 import Logout from "./components/logout"
+import ProtectedRoute from "./components/common/protected-route"
 import { ToastContainer } from "react-toastify"
 import { getCurrentUser } from "./services/authenticationService"
 import "react-toastify/dist/ReactToastify.css"
@@ -33,26 +34,16 @@ class App extends Component {
         <main className="container">
           <Switch>
             <Route
-              path="/movies/:id"
-              render={props => {
-                if (!user) return <Redirect to="/login" />
-                return <MovieForm {...props} />
-              }}
-            />
-            <Route
               path="/movies"
               render={props => <Movies user={user} {...props} />}
             />
-            <Route
-              path="/me"
-              render={props => <Profile user={user} {...props} />}
-            />
+            <ProtectedRoute path="/movies/:id" component={MovieForm} />
+            <ProtectedRoute path="/me" component={Profile} />} />
             <Route path="/customers" component={Customers} />
             <Route path="/rentals" component={Rentals} />
             <Route path="/login" component={LoginForm} />
             <Route path="/register" component={RegisterForm} />
             <Route path="/movie-form" component={MovieForm} />
-            } />
             <Route path="/logout" component={Logout} />
             <Route path="/not-found" component={NotFound} />
             <Redirect from="/" exact to="/movies" />
