edf056d0feafade5f1b26af8cde8bf764a4e93a5
Ario Tamaddoni <ariozone6@gmail.com>
2019-06-06 15:54:47 -0700

    Modify protectedRoute component to receive path and user dynamically.#38

--- src/components/common/protected-route.jsx
+++ src/components/common/protected-route.jsx
@@ -1,14 +1,15 @@
 import React from "react"
 import { Route, Redirect } from "react-router-dom"
 import { getCurrentUser } from "../../services/authenticationService"
-const ProtectedRoute = props => {
+const ProtectedRoute = ({ path, component: Component, render, ...rest }) => {
   const user = getCurrentUser()
   return (
     <Route
-      path={props.path}
+      {...rest}
+      path={path}
       render={props => {
         if (!user) return <Redirect to="/login" />
-        return <MovieForm {...props} />
+        return Component ? <Component {...props} /> : render(props)
       }}
     />
   )
