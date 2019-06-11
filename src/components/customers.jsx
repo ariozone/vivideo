c639243b4644a3d668b5e3fd3320bd5bdb78a5d2
Ario Tamaddoni <ariozone6@gmail.com>
2019-06-11 13:49:44 -0700

    Create a Customers class component and import require modules.#43

--- src/components/customers.jsx
+++ src/components/customers.jsx
@@ -1,6 +1,29 @@
 import React, { Component } from "react"
+import { Link } from "react-router-dom"
 
 export default class Customers extends Component {
+  state = { customers: [] }
+  columns = [
+    {
+      path: "name",
+      label: "Name",
+      content: customer => (
+        <Link className="text-dark" to={`/customers/${customer._id}`}>
+          {customer.name}
+        </Link>
+      )
+    },
+    { path: "contact", label: "Contact" },
+    {
+      key: "prime",
+      content: customer => (
+        <i
+          isPrime={customer.isPrime}
+          onClick={() => this.props.handlePrime(customer)}
+        />
+      )
+    }
+  ]
   render() {
     return <h1 className="my-5">Customers</h1>
   }
